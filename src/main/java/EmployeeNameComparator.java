a179739a516babadae1fbf1be098ccae4e79863f
yanary <yana.ryankel@temy.co>
2016-08-14 14:51:10 +0300

    Small fix - deleted Override

--- src/main/java/EmployeeNameComparator.java
+++ src/main/java/EmployeeNameComparator.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 
 public class EmployeeNameComparator implements Comparator<Employee> {
  
-    @Override
+   
     public int compare(Employee emp1, Employee emp2) {
       
         return emp1.getName().compareTo(emp2.getName());
